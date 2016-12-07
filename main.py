b6b54e07ec16e97d7c94d31f3bdd0245fb5ab127
Blakes <blakes22@gmail.com>
2016-12-07 07:52:53 +0100

    Add user selecting books handling.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 BASE_LINK = "http://www.allitebooks.com"
-SEARCH_ARG = "/?s="
-PAGE_ARG = "/page/{}"
+S_ARG = "/?s="
+P_ARG = "/page/{}"
 
 def read_links(filename):
     with open(filename, "r") as f:
@@ -20,12 +20,11 @@ def get_books_details(keyword, max_pages=None):
     keyword: e.g. <searched text>
     """
     
-    soup = BeautifulSoup(requests.get(BASE_LINK + SEARCH_ARG + keyword).text,
+    soup = BeautifulSoup(requests.get(BASE_LINK + S_ARG + keyword).text,
                                      "html.parser")
     
     # Assertion if searched keyword gave any results.
     if soup.find("h1", text = "No Posts Found."):
-        print("No books found.")
         return None
 
     # Defining number of pages to look through.
@@ -44,7 +43,7 @@ def get_books_details(keyword, max_pages=None):
     output_data = {}
     book_id = 1
     for i in range(max_pages):
-        link_page = BASE_LINK + PAGE_ARG.format(i+1) + SEARCH + keyword
+        link_page = BASE_LINK + P_ARG.format(i+1) + S_ARG + keyword
         soup = BeautifulSoup(requests.get(link_page).text, "html.parser")
         books = soup.findAll("article")
         for book in books:
@@ -60,10 +59,12 @@ def download_book(link):
     pdf_link = soup.find("span", class_ = "download-links").a['href']
     title = soup.find("h1", class_ = "single-title").text
     pdf_link = pdf_link.replace(" ", "%20")
+    print(pdf_link)
+    return;
     #TODO: Check following line
     #pdf_link = urllib.parse.quote(pdf_link, safe='')
     if os.path.exists(os.getcwd() + '\\' + title + '.pdf'):
-        print("File already exists:", title+".pdf")
+        print("File already exists:", title + ".pdf")
     else:
         print("Downloading file",  title + '.pdf...', end='')
         urllib.request.urlretrieve(pdf_link, title + ".pdf")
@@ -72,5 +73,53 @@ def download_book(link):
         print("Napping for", "{:3.2f}".format(nap) + "s")
         time.sleep(nap)
 
-  
+def show_books(books):
+    print("ID","Title")
+    for id in books:
+        title = books[id].split("/")[-2]
+        title = title.replace("-", " ").capitalize()
+        print(str(id) + ".", "\"" + title + "\".")
+
+def choose_books(books):
+    """Asks user to choose books IDs to download
+    Returns None if no ID was selected
+    Otherwise returns dictionary with pairs id:link of chosen books.
+    """
+    
+    response = input("Choose books to download (IDs seperated by commas) | [exit]: ")
+    if not response or response.lower() == "exit":
+        return None
+    response = response.split(",")
+    response = [abs(int(v.strip())) for v in response]
+    response = list(set(response))
+    output_data = {}
+    for id in response:
+        if id in books:
+            output_data[id] = books[id]
+    print("Selected {} books:".format(len(output_data)))
+    show_books(output_data)
+    return output_data
+books = get_books_details("python", 1)
+#TEMP FUNCTION
+def main():
+    #TODO: Add input handling here
+    #response = input("What books are you looking for: ")
+    #return urllib.parse.quote(response, safe='')
+    #response = get_input()
+    
+    if not books:
+        print("No books found.")
+    else:
+        show_books(books)
+        print()
+        try:
+            chosen_books = choose_books(books)
+            if not chosen_books:
+                print("byebye")
+        except ValueError as e:
+            print("Only numbers please.")
+    # Check if No results found
+    # Check how many pages there are found
+    # Ask if you want to proceed     
+main()
 
